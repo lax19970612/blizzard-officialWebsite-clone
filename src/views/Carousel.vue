bcc09d09316b7afb4d1b5d122ad1e97f1880d65e
Yao-Hsien Hsieh <laxative0612@gmail.com>
2020-02-27 15:46:58 +0800

    Environment, file structure, page build up
    Using vue-cli build up the whole project, setting up the project
    develop environment, and setting up the basic structure of the page.

---
+++ src/views/Carousel.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+#Carousel-container
+</template>
+
+<script>
+export default {
+  name: 'Carousel'
+}
+</script>
+
+<style scoped>
+#Carousel-container{
+  height:65vh;
+}
+</style>
---
+++ src/views/Pane.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  div
+</template>
+
+<script>
+export default {
+  name: 'Pane'
+}
+</script>
+
+<style scoped>
+
+</style>
