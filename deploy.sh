724fba428e5d5933d849916187bd5839c4edc470
Yao-Hsien Hsieh <laxative0612@gmail.com>
2021-04-19 20:34:08 +0800

    Add deploy shell

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+npm install
+# abort on errors
+set -e
+# build
+npm run build
+# navigate into the build output directory
+cd dist
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:laxative/blizzard-officialWebsite-clone.git master:gh-pages
+cd -
